72f268c1c989877d1ebc7b29c1a540613470c351
CruzanCaramele <letters2hamza@gmail.com>
2015-08-29 12:33:58 +0800

    files/index.html
    test file to ensire apache is working

--- scripts/centos-lamp.sh
+++ scripts/centos-lamp.sh
@@ -21,3 +21,18 @@ service httpd start
 
 # PHP
 
+yum install -y php php-cli php-common php-devel php-mysql
+
+# MySQL
+yum install -y mysql mysql-server mysql-devel
+
+chkconfig --add mysqld
+chkconfig mysqld on 
+
+service mysqld start
+
+mysql -u root -e "SHOW DATABASES";
+
+# Download Starter Content
+
+service httpd restart
