f6075ac0e6254eca59b18ee8a306d8bdf156b673
CruzanCaramele <letters2hamza@gmail.com>
2015-08-29 12:25:20 +0800

    remove html folder and set symbolic link betoween local vagrant folder and new html folder on centos

--- scripts/centos-lamp.sh
+++ scripts/centos-lamp.sh
@@ -3,6 +3,21 @@
 # exclude kernel, update centous with any patches
 yum update -y --exclude=kernel
 
+# tools
 yum install -y nano git unzip screen
 
+# apache
 yum install -y httpd httpd-devel httpd-tools
+
+chkconfig --add httpd
+chkconfig httpd on
+service httpd stop
+
+rm -rf /var/www/html
+
+ln -s /vagrant /var/www/html
+
+service httpd start
+
+# PHP
+
